85394edb8dd298b72c39476ddeea0ae2faeeb751
Fasna M.A. <fasnama1991@gmail.com>
2024-10-31 08:52:44 +0530

    navbar linked

--- django_tutorial/home/urls.py
+++ django_tutorial/home/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     
-    path('',views.index),
+    path('',views.index,name ='home'),
+    path('about/',views.about,name ='about'),
+    path('bookings/',views.bookings,name='bookings'),
+    path('doctors/',views.doctors,name='doctors'),
+    path('contact/', views.contact,name='contact'),
+    path('departments/',views.departments,name='departments'),
+
 ]
--- django_tutorial/home/views.py
+++ django_tutorial/home/views.py
@@ -4,3 +4,26 @@ from django.http  import HttpResponse
 def index(request):
     return  render(request,'index.html')
 
+
+def about(request):
+    return  render(request,'about.html')
+
+
+
+def bookings(request):
+    return render(request,'bookings.html')
+
+
+
+def  doctors(request):
+    return render(request,'doctors.html')
+
+
+
+def  departments(request):
+    return render(request,'departments.html')
+
+
+
+def contact(request):
+    return render(request,'contact.html')
