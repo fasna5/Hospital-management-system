d4310d7c5bdb9e25b62e63d5c446d8f0ab211b4e
Fasna M.A. <fasnama1991@gmail.com>
2024-11-02 17:11:14 +0530

    model2 doctors images added

--- django_tutorial/home/views.py
+++ django_tutorial/home/views.py
@@ -1,6 +1,6 @@
 from  django.shortcuts import render
 from django.http  import HttpResponse
-from . models import Departments
+from . models import Departments,Doctors
 
 def index(request):
     return  render(request,'index.html')
@@ -18,7 +18,10 @@ def bookings(request):
 
 
 def  doctors(request):
-    return render(request,'doctors.html')
+    dict_docs={
+        'doctors':Doctors.objects.all()
+    }
+    return render(request,'doctors.html',dict_docs)
 
 
 
